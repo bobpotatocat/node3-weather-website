03874342235e623f8b1f5e1de300ca3f90dd1a26
unknown <zlai005@e.ntu.edu.sg>
2020-04-21 16:29:19 +0700

    fixed humidity and feelslikE

--- src/utils/weather.js
+++ src/utils/weather.js
@@ -13,8 +13,8 @@ const weather =  (latitude, longitude, placeName, callback ) => {
             callback(undefined, {
                 currentTemperature : body.current.temperature,
                 weatherCondition : body.current.weather_descriptions,
-                feelsLikeTemp: body.current.weather_descriptions.feelslike,
-                humidity: body.current.weather_descriptions.humidity,
+                feelsLikeTemp: body.current.feelslike,
+                humidity: body.current.humidity,
                 placeName
             })
         }
