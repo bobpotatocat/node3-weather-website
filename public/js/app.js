12d593ea5da16539493a5e12b14c1dfee67e20e8
unknown <zlai005@e.ntu.edu.sg>
2020-04-21 15:54:08 +0700

    added some comment

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,9 @@
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 //messageOne.textContent = 'from javascript'
+//include some changes
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
